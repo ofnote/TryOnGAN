Fix metrics calculation

metrics/metric_utils.py
@@ -212,7 +212,7 @@ def compute_feature_stats_for_dataset(opts, detector_url, detector_kwargs, rel_l
 
     # Main loop.
     item_subset = [(i * opts.num_gpus + opts.rank) % num_items for i in range((num_items - 1) // opts.num_gpus + 1)]
-    for images, _labels, pose in torch.utils.data.DataLoader(dataset=dataset, sampler=item_subset, batch_size=batch_size, **data_loader_kwargs):
+    for images, _pmap, _labels, pose in torch.utils.data.DataLoader(dataset=dataset, sampler=item_subset, batch_size=batch_size, **data_loader_kwargs):
         if images.shape[1] == 1:
             images = images.repeat([1, 3, 1, 1])
         features = detector(images.to(opts.device), **detector_kwargs)
@@ -265,7 +265,7 @@ def compute_feature_stats_for_generator(opts, detector_url, detector_kwargs, rel
             c = [dataset.get_label(np.random.randint(len(dataset))) for _i in range(batch_gen)]
             c = torch.from_numpy(np.stack(c)).pin_memory().to(opts.device)
             randidx = [np.random.randint(len(dataset)) for _i in range(batch_gen)]
-            pose = [dataset.__getitem__(idx)[2] for idx in randidx]
+            pose = [dataset.__getitem__(idx)[3] for idx in randidx]
             pose = torch.stack(pose).to(opts.device)
             images.append(run_generator(z, c, pose))
         images = torch.cat(images)